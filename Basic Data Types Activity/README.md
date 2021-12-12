fb0d501e74e535d028c1500c8c6b120a88d21d8e
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2021-12-12 12:07:13 -0500

    new file

--- Basic Data Types Activity/README.md
+++ Basic Data Types Activity/README.md
@@ -1,1 +1,2 @@
 # Working with Basic Data Types Activity
+In this activity, I had to use arrays and different types of variables(strings and ints) to make a story. 
---
+++ Basic Data Types Activity/story_timeKalyanMondal.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let name = "fisherman guy";
+let location = "THE lake";
+let vehicle = "large boat";
+let occupation = "farmer";
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = -2020;
+let litresOfJuice = 5000000;
+let angleOfToiletBowl = 90;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponsOfMassDestruction = ["piece of a rotten tomato","a large crate of rotten tomotoes","broken nerf gun","nuke"]; 
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let isCompetent = false;
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Welcome to "+location);
+console.log("The year is "+year+" and your name is "+name);
+console.log("Your job is "+occupation+". Oh no, you are late, you must go to your boat to get to the juice farm.\n");
+console.log("Last year you cultivated "+litresOfJuice+" litres of juice. You turn the toilet bowl that guides your vessel to "+angleOfToiletBowl+" degrees.\n");
+console.log("You whip out your "+weaponsOfMassDestruction[2]+" to eliminate a fish and successfully allow it to not breathe anymore.\n");
+console.log("Are you competent? "+isCompetent+"\n");
+console.log("To celebrate, you take out "+weaponsOfMassDestruction[1]+" from your pocket and eat a "+weaponsOfMassDestruction[0]);
+console.log("you cry about your life...");
+console.log("Depressed, you become a narcissistic evil criminal you sets of a "+weaponsOfMassDestruction[3]);
+console.log("You did it out of love! ❤️❤️❤️❤️❤️❤️❤️❤️");
+console.log("❤️❤️THE❤️❤️END❤️❤️");
